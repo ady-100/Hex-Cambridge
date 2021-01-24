7c5ace6ae341c7391584f01a9c1a48db350f2042
or280 <77881012+or280@users.noreply.github.com>
2021-01-24 08:47:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -345,44 +345,11 @@ def productadd():
         costpy = float(request.form['cost'])
         weightpy = float(request.form['weight'])
     
-        with open('Countries_Data.csv', newline='') as csvfile:
-            csvdata = csv.reader(csvfile, delimiter=',')
-            co_data = {}
-            for row in csvdata:
-                co_data[row[0]] = [float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5])]
-    
-        with open('Materials_Data.csv', newline='') as csvfile:
-            csvdata = csv.reader(csvfile, delimiter=',')
-            mat_data = {}
-            for row in csvdata:
-                mat_data[row[0]] = [float(row[1])]
-    
-        # Environmental Score
-        
-        envmat1 = (((mat_data[str(material1py)][0])*percent1py)+((mat_data[str(material2py)][0])*percent2py))
-        envmat = envmat1*weightpy/100
-        envco = weightpy * 150e-6 * (co_data[countrypy][4])
-        Environ = envmat + envco 
-    
-        # Ethical Score
-    
-        ethmat = 0
-        ethco = weightpy*((0.5)*((co_data[countrypy][0])/15)+(2/9)*((1-co_data[countrypy][1]+co_data[countrypy][2]+co_data[countrypy][3])/100))*100
-        Ethical = ethmat + ethco
-    
-        # Final Score
-        Score = Ethical/10 + 100/Environ
-    
-        # Score per kg
-        kgscore = Score/weightpy
-    
-        # Price per weight adjusted score
-        cost_effectivness = kgscore/float(costpy)
-        colour = colourcode(kgscore)
+	A = Algorithm(countrypy, material1py, percent1py, material2py, percent2py, costpy, weightpy)
         
         # Add user to database
         username = session["username"]
-        c.execute("INSERT INTO products (username, productname, country, material1, percentage1, material2, percentage2, cost, weight, score) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (username, productname, countrypy, material1py, percent1py, material2py, percent2py, costpy, weightpy, Score))
+        c.execute("INSERT INTO products (username, productname, country, material1, percentage1, material2, percentage2, cost, weight, score) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (username, productname, countrypy, material1py, percent1py, material2py, percent2py, costpy, weightpy, A[2]))
         
         # Save commit
         conn.commit()
