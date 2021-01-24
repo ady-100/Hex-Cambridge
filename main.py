e359af2135314c8be2e3bd8028466ca97a4c7193
Kieran Dalton <kierandalton@live.com>
2021-01-24 09:51:17 +0000

    Update main.py

--- main.py
+++ main.py
@@ -322,7 +322,7 @@ def products():
         percentage2 = item["percentage2"]
         weight = item["weight"]
         score = item["score"]
-        colour = colourcode(score/weight)
+        colour = colourcode(float(score)/float(weight))
         
         an_item = dict(productname = productname, cost= cost, country= country, material1= material1, percentage1= percentage1, material2= material2, percentage2= percentage2, weight= weight, score= score, colour= colour)
         product_list_display.append(an_item)
