24024bb0ad33eef33f7a11ff8b574f1dfd17d14e
Kieran Dalton <kierandalton@live.com>
2021-01-24 07:59:46 +0000

    Revert "Update main.py"
    This reverts commit b89059fdeb014a700e19bb4e889461596d9db7b2.

--- main.py
+++ main.py
@@ -217,41 +217,47 @@ def colourcode(value):
         return "Green"
 
 def Algorithm(country, material1, percent1, material2, percent2, cost, weight):
-    with open('Countries_Data.csv', newline='') as csvfile:
-        csvdata = csv.reader(csvfile, delimiter=',')
-        co_data = {}
-        for row in csvdata:
-            co_data[row[0]] = [float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5])]
-    with open('Materials_Data.csv', newline='') as csvfile:
-        csvdata = csv.reader(csvfile, delimiter=',')
-        mat_data = {}
-        for row in csvdata:
-            mat_data[row[0]] = [float(row[1])]
-		# Environmental Score
-        envmat1 = (((mat_data[str(material1)][0])*percent1)+((mat_data[str(material2)][0])*percent2))
-        envmat = envmat1*weight/100
-        envco = weight * 150e-6 * (co_data[country][4])
-        Environ = round(envmat + envco,2) 
+	with open('Countries_Data.csv', newline='') as csvfile:
+		csvdata = csv.reader(csvfile, delimiter=',')
+		co_data = {}
+		for row in csvdata:
+		    co_data[row[0]] = [float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5])]
+
+	with open('Materials_Data.csv', newline='') as csvfile:
+		csvdata = csv.reader(csvfile, delimiter=',')
+		mat_data = {}
+		for row in csvdata:
+		    mat_data[row[0]] = [float(row[1])]
+		
+
+	    # Environmental Score
+    
+	    envmat1 = (((mat_data[str(material1)][0])*percent1)+((mat_data[str(material2)][0])*percent2))
+	    envmat = envmat1*weight/100
+	    envco = weight * 150e-6 * (co_data[country][4])
+	    Environ = round(envmat + envco,2) 
 
 	    # Ethical Score
 
-        ethmat = 0
-        ethco = weight*((0.5)*((co_data[country][0])/15)+(2/9)*((1-co_data[country][1]+co_data[country][2]+co_data[country][3])/100))*100
-        Ethical = ethmat + ethco
+	    ethmat = 0
+	    ethco = weight*((0.5)*((co_data[country][0])/15)+(2/9)*((1-co_data[country][1]+co_data[country][2]+co_data[country][3])/100))*100
+	    Ethical = ethmat + ethco
 
 	    # Final Score
-        Score = round(Environ + 200*weight/Ethical,2)
+	    Score = round(Environ + 200*weight/Ethical,2)
 
 	    # Score per kg
-        kgscore = round(Score/weight,2)
+	    kgscore = round(Score/weight,2)
 
 	    # Price per weight adjusted score
-        cost_effectivness = round(kgscore/float(cost),2)
+	    cost_effectivness = round(kgscore/float(cost),2)
 
-        colour = colourcode(kgscore)
-        Al = [Environ, Ethical, Score, kgscore, cost_effectivness, colour]
+	    colour = colourcode(kgscore)
+	
+	    Al = [Environ, Ethical, Score, kgscore, cost_effectivness, colour]
 		
-        return Al
+	    return Al
+
 
 
 @app.route('/data', methods=['POST'])
