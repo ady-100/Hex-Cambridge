4991119255c3f5cace66d034d775974e7ae8d25f
or280 <77881012+or280@users.noreply.github.com>
2021-01-24 03:27:15 +0000

    Update ignore.py

--- templates/ignore.py
+++ templates/ignore.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/ignore.py', methods=['GET', 'POST'])
+@app.route('/contact.html', methods=['GET', 'POST'])
 def hello():
     return render_template('contact.html', say=request.form['email'], to=request.form['query'])
 
